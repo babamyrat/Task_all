fc0d1ed846457d6aec240988bf8e68e7999e1d12
babamyrat <-l>
2021-07-11 17:18:39 +0300

    Task 6

---
+++ src/main/java/Lesson6/Cat.java
@@ -0,0 +1,8 @@
+package Lesson6;
+
+class Cat extends Animal {
+
+    Cat(double animalDistanceRun, double animalDistanceJump, double animalDistanceSwim) {
+        super(animalDistanceRun, animalDistanceJump, animalDistanceSwim);
+    }
+}
---
+++ src/main/java/Lesson6/Interface.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+public interface Interface {
+
+    boolean run(double value);
+
+    boolean jump(double value);
+
+    boolean swim(double value);
+
+}
