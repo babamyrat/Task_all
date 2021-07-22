59f29143f64e2a084748792a8d9818055e350bfd
tmtal <tmstudentler@gmail.com>
2021-07-22 06:58:28 +0300

    Task9

---
+++ src/main/java/Lesson9/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package Lesson9;
+
+
+    public class MyArraySizeException extends Exception {
+//        MyArraySizeException(String message) {
+//            super(message);
+//        }
+
+    }
+
