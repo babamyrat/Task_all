8d03f2e5223266586a228640d4deecf6dd1004b5
Admin <tmstudentler@gmail.com>
2021-07-28 11:48:35 +0300

    Task11

---
+++ src/main/java/Lesson11/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson11;
+
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
