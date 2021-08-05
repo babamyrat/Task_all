b4224bfd57732609e34b8585efa1b299a241d27e
Admin <tmstudentler@gmail.com>
2021-08-05 19:38:20 +0300

    Task13

---
+++ src/main/java/Lesson13/MainActivity.java
@@ -0,0 +1,29 @@
+package Lesson13;
+
+import java.util.concurrent.*;
+
+public class MainActivity {
+
+    static final int CARS_COUNT = 4;
+    static final CountDownLatch countDownLatchFinish = new CountDownLatch(CARS_COUNT);
+    static final CountDownLatch countDownLatchReady = new CountDownLatch(CARS_COUNT);
+    static final CyclicBarrier startBarrier = new CyclicBarrier(CARS_COUNT);
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(80), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            final int randomSpeed = 20 + (int) (Math.random() * 10);
+            cars[i] = new Car(race, randomSpeed);
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+        countDownLatchReady.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        countDownLatchFinish.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+}
